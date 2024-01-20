f205d112af09b731470938ca87853a3f11348bd2
Ujjawal360 <ujjawalshah360@gmail.com>
2024-01-19 22:49:47 -0500

    The first assignment.

---
+++ LSP_spring2024_assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Ujjawal Shah
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
